6ed84fe8a30b5149f8ba58ea00a38248c45303e4
Selim Gökçek <selimgokcke@outlook.com>
2019-08-04 22:45:01 +0300

    Dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -70,6 +70,100 @@ Vue.component('Dashboard',
           })
       },
       mounted() {
+        const ctx1 = document.getElementById('playersChart');
+        const playersChart = new Chart(ctx1, {
+          type: 'line',
+          data: {
+            labels: ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"],
+            datasets: [{
+              data: [8, 5, 9, 7, 12, 8, 9],
+              backgroundColor: 'rgba(72, 207, 173, .05)',
+              borderColor: '#48CFAD',
+              borderWidth: 2,
+              pointRadius: 0,
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: false
+                },
+                display: false
+              }],
+              xAxes: [{
+                display: false
+              }],
+            },
+            legend: {
+              display: false,
+            }
+          }
+        });
+
+        const ctx2 = document.getElementById('ticketsChart');
+        const ticketsChart = new Chart(ctx2, {
+          type: 'line',
+          data: {
+            labels: ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"],
+            datasets: [{
+              data: [8, 5, 9, 7, 12, 8, 9],
+              backgroundColor: 'rgba(252, 110, 81, .05)',
+              borderColor: '#fc6e51',
+              borderWidth: 2,
+              pointRadius: 0,
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: false
+                },
+                display: false
+              }],
+              xAxes: [{
+                display: false
+              }],
+            },
+            legend: {
+              display: false,
+            }
+          }
+        });
+
+
+        const ctx3 = document.getElementById('postsChart');
+        const postsChart = new Chart(ctx3, {
+          type: 'line',
+          data: {
+            labels: ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"],
+            datasets: [{
+              data: [8, 5, 9, 7, 12, 8, 9],
+              backgroundColor: 'rgba(246, 187, 66, .05)',
+              borderColor: '#F6BB42',
+              borderWidth: 2,
+              pointRadius: 0,
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: false
+                },
+                display: false
+              }],
+              xAxes: [{
+                display: false
+              }],
+            },
+            legend: {
+              display: false,
+            }
+          }
+        });
+
         const ctx = document.getElementById('myChart');
         const myChart = new Chart(ctx, {
           type: 'line',
@@ -77,9 +171,9 @@ Vue.component('Dashboard',
             labels: ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"],
             datasets: [{
               data: [39, 45, 83, 53, 89, 72, 99],
-              backgroundColor: 'rgba(0, 123, 255, .1)',
+              backgroundColor: 'rgba(0, 123, 255, .05)',
               borderColor: '#007bff',
-              borderWidth: 3,
+              borderWidth: 2.5,
             }]
           },
           options: {
