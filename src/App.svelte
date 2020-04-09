5141a5aada634bd5b7d0d4893ecaac8c1cd78165
kahverengi <ahmet.duruer@yandex.com>
2020-04-09 22:53:29 +0300

    Add splash screen

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
   import page from "page"
   import {ChunkGenerator} from "svelte-spa-chunk";
 
+  import {basePath} from "./util/path.util";
+
   import ChunkComponent from "./Chunk.svelte";
   import {isPageLoading} from "./ChunkStore";
 
@@ -11,6 +13,7 @@
   const Chunk = ChunkGenerator(ChunkComponent);
 
   let props = {};
+  let showSplash = true;
 
   page.base("/panel")
 
@@ -33,8 +36,46 @@
   @import "styles/main";
 </style>
 
-{#if $isPageLoading}
-  <PageLoading/>
+<!-- Splash Animation -->
+{#if showSplash}
+  <div class="d-flex justify-content-center align-items-center w-100 min-vh-100" role="status">
+    <img class="animated fadeIn infinite slow" src={basePath() + "assets/img/logo-blue.svg"} style="visibility: hidden;"
+         width="32">
+  </div>
 {/if}
+
+  <!-- Main Contents Hidden -->
+<!--{#if !showSplash}-->
+<div hidden={showSplash}>
+  <!--  :class="{ 'd-flex': !showSplash }"-->
+  <!--  <Main v-show="!showSplash"></Main>-->
+
+
+  <!--  <Sidebar></Sidebar>-->
+  <!--  Main  -->
+  <main class="panel-content">
+    <!--    <Navbar></Navbar>-->
+
+    <!-- Content  -->
+    <!--    <router-view-->
+    <!--            v-show="!routePageLoading && !initialPageDataLoading"-->
+    <!--    ></router-view>-->
+    <svelte:component this={props.component} {...props}/>
+
+    <!--    <div-->
+    <!--            :class="{ 'd-flex': routePageLoading || initialPageDataLoading }"-->
+    <!--            class="align-items-center justify-content-center w-100 h-75"-->
+    <!--            v-show="routePageLoading"-->
+    <!--    >-->
+    <!--      <div class="spinner-border text-primary" role="status"></div>-->
+    <!--    </div>-->
+  </main>
+</div>
+<!--{/if}-->
+
+
+  <!--{#if $isPageLoading}-->
+<!--  <PageLoading/>-->
+<!--{/if}-->
 
 <svelte:component this={props.component} {...props}/>
