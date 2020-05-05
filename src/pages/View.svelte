c578a91f1702a84178fedc8e4ab866c639189b4b
kahverengi <ahmet.duruer@yandex.com>
2020-05-06 00:12:52 +0300

    Add router to view page

--- src/pages/View.svelte
+++ src/pages/View.svelte
@@ -1,7 +1,32 @@
 <script>
-  import {isPageInitialized} from "../Store"
+  import Router from "../components/Router.svelte";
+  import {path} from "../RouterStore";
 
-  isPageInitialized.set(true);
+  function matching(path, pathName, startsWith = false) {
+    return (
+      path.toUpperCase() === pathName.toUpperCase() ||
+      path.toUpperCase() === (pathName + "/").toUpperCase() ||
+      (startsWith && path.startsWith(pathName))
+    );
+  }
 </script>
 
-<div class="content">View page!</div>
+<!-- View Page -->
+
+<!-- View Categories Navigation -->
+<nav class="border-bottom border-glass page-category">
+  <div class="nav nav-pills d-flex flex-row w-100">
+    <a class="nav-item nav-link" href="/panel/view" class:active={matching($path, '/panel/view')}>
+      Temalar
+    </a>
+
+    <a class="nav-item nav-link" href="/panel/view/theme-options" class:active={matching($path, '/panel/view/theme-options')}>
+      Tema Seçenekleri
+    </a>
+  </div>
+</nav>
+
+<!-- Settings Contents -->
+<article class="content">
+  <Router basePath="/panel/view"/>
+</article>
