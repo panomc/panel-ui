f33b7151a9131f234d9f599dca5f1e2eccaef5f0
Selim Gökçek <selimgokcek@outlook.com>
2020-05-08 21:25:38 +0300

    Fix svelte warnings

--- src/pages/Tickets/AllTickets.svelte
+++ src/pages/Tickets/AllTickets.svelte
@@ -1,5 +1,8 @@
 <script>
   import { isPageInitialized } from "../../Store";
+  import Icon from "svelte-awesome";
+  import { faListAlt } from "@fortawesome/free-regular-svg-icons";
+  import { faTicketAlt } from "@fortawesome/free-solid-svg-icons";
 
   import ConfirmCloseTicketModal from "../../components/modals/ConfirmCloseTicketModal.svelte";
   isPageInitialized.set(true);
@@ -9,14 +12,10 @@
 
 <div class="row mb-3">
   <div class="col-md-4 col-6">
-    <router-link
-      class="btn btn-link"
-      role="button"
-      to="/panel/tickets/categories"
-    >
-      <i aria-hidden="true" class="far fa-list-alt fa-fw"></i>
-      <span class="d-md-inline d-none">Kategoriler</span>
-    </router-link>
+    <a class="btn btn-link" role="button" href="/panel/tickets/categories">
+      <Icon data="{faListAlt}" class="mr-1" />
+      Kategoriler
+    </a>
   </div>
 </div>
 
@@ -36,36 +35,36 @@
       <div class="col-md-6 col-12 text-md-right text-center">
         <div class="btn-group">
           <!--            :class="{ 'active': page_type === 'all' }"-->
-          <router-link
+          <a
             class="btn btn-sm btn-outline-light btn-link"
             role="button"
-            to="/panel/tickets/all"
+            href="/panel/tickets/all"
           >
             Tümü
-          </router-link>
+          </a>
           <!--            :class="{ 'active': page_type === 'waitingReply' }"-->
-          <router-link
+          <a
             class="btn btn-sm btn-outline-light btn-link"
             role="button"
-            to="/panel/tickets/waitingReply"
+            href="/panel/tickets/waitingReply"
           >
             Yanıtlanmadı
-          </router-link>
+          </a>
           <!--            :class="{ 'active': page_type === 'closed' }"-->
-          <router-link
+          <a
             class="btn btn-sm btn-outline-light btn-link text-danger"
             role="button"
-            to="/panel/tickets/closed"
+            href="/panel/tickets/closed"
           >
             Kapalı
-          </router-link>
+          </a>
         </div>
       </div>
     </div>
 
     <!-- No Tickets -->
     <div class="container text-center" v-if="tickets_count === 0">
-      <i aria-hidden="true" class="fa fa-ticket-alt fa-4x text-glass m-3"></i>
+      <Icon data="{faTicketAlt}" scale="3" class="text-glass m-3" />
       <p class="text-gray">Burada içerik yok.</p>
     </div>
 
@@ -104,12 +103,10 @@
               <b class="text-muted" title="Talep NO">#</b>
             </td>
             <td>
-              <router-link
-                :to="'/panel/tickets/' + ticket.id"
-                title="Talebi Görüntüle"
-              >
+              <!-- :href="'/panel/tickets/' + ticket.id" -->
+              <a href="/panel/tickets/" title="Talebi Görüntüle">
                 <!--                  {{ ticket.title }}-->
-              </router-link>
+              </a>
             </td>
             <td>
               <span
@@ -161,7 +158,8 @@
             <!--                {{index}}-->
           </a>
           <!--            {{index}}-->
-          <a class="page-link" v-if="page === index"></a>
+          <a class="page-link" href="#">?</a>
+          <!-- v-if="page === index" -->
         </li>
 
         <!--          :class="{ 'disabled': page === total_page }"-->
