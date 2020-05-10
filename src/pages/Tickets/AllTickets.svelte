af265f92ab765ec65215d465fadcc80df89195c7
kahverengi <ahmet.duruer@yandex.com>
2020-05-10 16:04:44 +0300

    Add tickets page API and UI logics

--- src/pages/Tickets/AllTickets.svelte
+++ src/pages/Tickets/AllTickets.svelte
@@ -1,11 +1,105 @@
 <script>
-  import { isPageInitialized } from "../../Store";
+  import ConfirmCloseTicketModal from "../../components/modals/ConfirmCloseTicketModal.svelte";
+
+  import { isPageInitialized, showNetworkErrorOnCatch } from "../../Store";
+  import ApiUtil from "../../util/api.util";
+  import { basePageInstance, path } from "../../RouterStore";
+
   import Icon from "svelte-awesome";
   import { faListAlt } from "@fortawesome/free-regular-svg-icons";
   import { faTicketAlt } from "@fortawesome/free-solid-svg-icons";
 
-  import ConfirmCloseTicketModal from "../../components/modals/ConfirmCloseTicketModal.svelte";
-  isPageInitialized.set(true);
+  import { get } from "svelte/store";
+
+  export let page = undefined;
+  export let pageType = "all";
+
+  let ticketsCount = 0;
+  let tickets = [];
+  let totalPage = 1;
+  let pages = [];
+
+  function getStatusFromPageType() {
+    return pageType === "all" ? 1 : pageType === "waitingReply" ? 2 : 0;
+  }
+
+  function routePage(pageNumber, forceReload = false) {
+    console.log("RoutePage called" + pageNumber);
+    if (pageNumber !== page || forceReload) {
+      showNetworkErrorOnCatch(
+        () =>
+          new Promise((resolve, reject) => {
+            ApiUtil.post("panel/initPage/ticketPage", {
+              page: pageNumber,
+              page_type: getStatusFromPageType(),
+            })
+              .then((response) => {
+                if (response.data.result === "ok") {
+                  ticketsCount = response.data.tickets_count;
+                  tickets = response.data.tickets;
+                  totalPage = response.data.total_page;
+
+                  pages = [];
+
+                  for (let i = 1; i <= totalPage; i++) {
+                    pages.push(i);
+                  }
+
+                  page = pageNumber;
+
+                  if (
+                    page === 1 &&
+                    get(path) !== "/panel/tickets" &&
+                    get(path) !== "/panel/tickets/" &&
+                    get(path) !== "/panel/tickets/" + pageType &&
+                    get(path) !== "/panel/tickets/" + pageType + "/"
+                  )
+                    basePageInstance("/panel/tickets/" + pageType + "/" + page);
+                  else if (page !== 1)
+                    basePageInstance("/panel/tickets/" + pageType + "/" + page);
+
+                  isPageInitialized.set(true);
+
+                  resolve();
+                } else if (response.data.result === "error") {
+                  const errorCode = response.data.error;
+
+                  isPageInitialized.set(true);
+
+                  if (errorCode === "PAGE_NOT_FOUND") {
+                    basePageInstance("/panel/error-404");
+                  }
+
+                  resolve();
+                } else reject();
+              })
+              .catch(() => {
+                reject();
+              });
+          })
+      );
+    }
+  }
+
+  function onLastPageClick() {
+    routePage(totalPage);
+  }
+
+  function onFirstPageClick() {
+    routePage(1);
+  }
+
+  function onPageLinkClick(index) {
+    routePage(index);
+  }
+
+  function checkPageLinkCurrentPage(index) {
+    return page !== index;
+  }
+
+  routePage(
+    typeof page === "undefined" ? 1 : parseInt(page) === 0 ? 1 : parseInt(page)
+  );
 </script>
 
 <!-- Tickets Page -->
@@ -25,34 +119,32 @@
     <div class="row justify-content-between">
       <div class="col-md-6 col-12 text-md-left text-center">
         <h5 class="card-title">
-          <!--              {{ tickets_count }} -->
-          Talep -
-          <!--            {{ page }}-->
-          <!--            {{ total_page }}-->
+          {ticketsCount} Talep - {page}
           <span class="text-primary">/</span>
+          {totalPage}
         </h5>
       </div>
       <div class="col-md-6 col-12 text-md-right text-center">
         <div class="btn-group">
-          <!--            :class="{ 'active': page_type === 'all' }"-->
           <a
             class="btn btn-sm btn-outline-light btn-link"
+            class:active="{pageType === 'all'}"
             role="button"
             href="/panel/tickets/all"
           >
             Tümü
           </a>
-          <!--            :class="{ 'active': page_type === 'waitingReply' }"-->
           <a
             class="btn btn-sm btn-outline-light btn-link"
+            class:active="{pageType === 'waitingReply'}"
             role="button"
             href="/panel/tickets/waitingReply"
           >
             Yanıtlanmadı
           </a>
-          <!--            :class="{ 'active': page_type === 'closed' }"-->
           <a
             class="btn btn-sm btn-outline-light btn-link text-danger"
+            class:active="{pageType === 'closed'}"
             role="button"
             href="/panel/tickets/closed"
           >
@@ -63,112 +155,131 @@
     </div>
 
     <!-- No Tickets -->
-    <div class="container text-center" v-if="tickets_count === 0">
-      <Icon data="{faTicketAlt}" scale="3" class="text-glass m-3" />
-      <p class="text-gray">Burada içerik yok.</p>
-    </div>
-
-    <!-- Tickets Table -->
-    <div class="table-responsive" v-if="tickets_count !== 0">
-      <table class="table mb-0">
-        <thead>
-          <tr>
-            <th scope="col">
-              <a href="#">Seç</a>
-            </th>
-            <th scope="col">Talep</th>
-            <th class="min-w-200px" scope="col">Konu</th>
-            <th scope="col">Durum</th>
-            <th scope="col">Kategori</th>
-            <th scope="col">Yazan</th>
-            <th scope="col">Son Yanıt</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr :key="index" v-for="(ticket, index) in tickets">
-            <th scope="row">
-              <div class="d-flex flex-row align-items-center">
-                <div class="custom-control custom-checkbox">
-                  <input
-                    class="custom-control-input"
-                    id="postCheck"
-                    type="checkbox"
-                  />
-                  <label class="custom-control-label" for="postCheck"></label>
-                </div>
-              </div>
-            </th>
-            <td>
-              <!--              {{ ticket.id }}-->
-              <b class="text-muted" title="Talep NO">#</b>
-            </td>
-            <td>
-              <!-- :href="'/panel/tickets/' + ticket.id" -->
-              <a href="/panel/tickets/" title="Talebi Görüntüle">
-                <!--                  {{ ticket.title }}-->
-              </a>
-            </td>
-            <td>
-              <span
-                class="text-center rounded border"
-                v-if="ticket.status === 1"
-              >
-                Yanıtlandı
-              </span>
-              <span
-                class="text-center rounded bg-success text-white"
-                v-if="ticket.status === 2"
-              >
-                Yeni
-              </span>
-              <span
-                class="text-center rounded bg-danger text-white"
-                v-if="ticket.status === 0"
-              >
-                Kapalı
-              </span>
-            </td>
-            <!--            {{ ticket.category.title }}-->
-            <td></td>
-            <!--            {{ ticket.writer.username }}-->
-            <td></td>
-            <td>1 saat önce</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
+    {#if ticketsCount === 0}
+      <div class="container text-center">
+        <Icon data="{faTicketAlt}" scale="3" class="text-glass m-3" />
+        <p class="text-gray">Burada içerik yok.</p>
+      </div>
+    {:else}
+      <!-- Tickets Table -->
+      <div class="table-responsive">
+        <table class="table mb-0">
+          <thead>
+            <tr>
+              <th scope="col">
+                <a href="#">Seç</a>
+              </th>
+              <th scope="col">Talep</th>
+              <th class="min-w-200px" scope="col">Konu</th>
+              <th scope="col">Durum</th>
+              <th scope="col">Kategori</th>
+              <th scope="col">Yazan</th>
+              <th scope="col">Son Yanıt</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each tickets as ticket, index (ticket)}
+              <tr>
+                <th scope="row">
+                  <div class="d-flex flex-row align-items-center">
+                    <div class="custom-control custom-checkbox">
+                      <input
+                        class="custom-control-input"
+                        id="postCheck{ticket.id}"
+                        type="checkbox"
+                      />
+                      <label
+                        class="custom-control-label"
+                        for="postCheck{ticket.id}"
+                      ></label>
+                    </div>
+                  </div>
+                </th>
+                <td>
+                  <b class="text-muted" title="Talep NO">#{ticket.id}</b>
+                </td>
+                <td>
+                  <a href="/panel/tickets/{ticket.id}" title="Talebi Görüntüle">
+                    {ticket.title}
+                  </a>
+                </td>
+                <td>
+                  {#if ticket.status === 1}
+                    <span class="text-center rounded border">Yanıtlandı</span>
+                  {:else if ticket.status === 2}
+                    <span class="text-center rounded bg-success text-white">
+                      Yeni
+                    </span>
+                  {:else if ticket.status === 0}
+                    <span class="text-center rounded bg-danger text-white">
+                      Kapalı
+                    </span>
+                  {/if}
+                </td>
+                <td>{ticket.category.title}</td>
+                <td>{ticket.writer.username}</td>
+                <td>1 saat önce</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+    {/if}
     <!-- Pagination -->
     <nav class="pt-3">
       <ul class="pagination pagination-sm mb-0 justify-content-start">
-        <!--          :class="{ 'disabled': page === 1 }"-->
-        <!--          @click="routePage(1)"-->
-        <li class="page-item">
+        <li
+          class="page-item"
+          class:disabled="{page === 1}"
+          disabled="{page === 1}"
+          on:click="{onFirstPageClick}"
+        >
           <a class="page-link" href="javascript:void(0);" title="Önceki Sayfa">
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
 
-        <!--          :class="{ 'active': index === page }"-->
-        <!--          :key="index"-->
-        <!--          @click="routePage(index)"-->
-        <!--          v-for="index in total_page"-->
-        <li class="page-item">
-          <a class="page-link" href="javascript:void(0);" v-if="page !== index">
-            <!--                {{index}}-->
-          </a>
-          <!--            {{index}}-->
-          <a class="page-link" href="#">?</a>
-          <!-- v-if="page === index" -->
-        </li>
+        {#each pages as index}
+          {#if checkPageLinkCurrentPage(index)}
+            <a href="javascript:void(0);" on:click="{onPageLinkClick(index)}">
+              <li class="page-item page-link">{index}</li>
+            </a>
+          {:else}
+            <li class="page-item page-link">{index}</li>
+          {/if}
+        {/each}
 
-        <!--          :class="{ 'disabled': page === total_page }"-->
-        <!--          @click="routePage(total_page)"-->
-        <li class="page-item">
-          <a class="page-link" href="javascript:void(0);" title="Sonraki Sayfa">
-            <span aria-hidden="true">&raquo;</span>
+        {#if checkPageLinkCurrentPage(index)}
+          <a
+                  href="javascript:void(0);"
+                  title="Sonraki Sayfa"
+          >
+            <li
+                    class="page-item page-link"
+                    class:disabled="{page === totalPage}"
+                    disabled="{page === totalPage}"
+                    on:click="{onLastPageClick}"
+            >
+
+              <span aria-hidden="true">&raquo;</span>
+            </li>
           </a>
-        </li>
+        {:else}
+          <a
+                  href="javascript:void(0);"
+                  title="Sonraki Sayfa"
+          >
+            <li
+                    class="page-item page-link"
+                    class:disabled="{page === totalPage}"
+                    disabled="{page === totalPage}"
+                    on:click="{onLastPageClick}"
+            >
+
+              <span aria-hidden="true">&raquo;</span>
+            </li>
+          </a>
+        {/if}
       </ul>
     </nav>
   </div>
