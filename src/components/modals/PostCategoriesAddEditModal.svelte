3eaa38802fcb58e4d1c30d12c51f773db6978477
kahverengi <ahmet.duruer@yandex.com>
2020-10-23 20:42:09 +0200

    Add onHide event to PostCategoriesAddEditModal

--- src/components/modals/PostCategoriesAddEditModal.svelte
+++ src/components/modals/PostCategoriesAddEditModal.svelte
@@ -6,11 +6,19 @@
   const mode = writable("create");
   const category = writable({});
   const errors = writable([]);
+
   let callback = (routeFirstPage) => {};
+  let hideCallback = (category) => {};
 
   export function show(
     newMode,
-    newCategory = { id: -1, title: "", description: "", url: "", color: "#1976d2" }
+    newCategory = {
+      id: -1,
+      title: "",
+      description: "",
+      url: "",
+      color: "#1976d2",
+    }
   ) {
     mode.set(newMode);
 
@@ -18,17 +26,23 @@
       newCategory.color = "#" + newCategory.color;
 
     category.set(newCategory);
-    errors.set([])
+    errors.set([]);
 
-    jquery("#" + dialogID).modal("show");
+    jquery("#" + dialogID).modal({backdrop: 'static', keyboard: false});
   }
 
   export function hide() {
+    if (get(mode) === "edit") hideCallback(get(category));
+
     jquery("#" + dialogID).modal("hide");
   }
 
   export function setCallback(newCallback) {
     callback = newCallback;
+  }
+
+  export function onHide(newCallback) {
+    hideCallback = newCallback;
   }
 </script>
 
@@ -88,9 +102,9 @@
         <button
           aria-label="Kapat"
           class="close"
-          data-dismiss="modal"
           title="Pencereyi Kapat"
           type="button"
+          on:click="{hide}"
         >
           <span aria-hidden="true">&times;</span>
         </button>
