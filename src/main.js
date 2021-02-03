ace3cfa978100e059294bff50cee00db9759435f
kahverengi <ahmet.duruer@yandex.com>
2021-02-03 20:25:16 +0300

    Remove unnecessary jQuery and PopperJS implementation in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import jquery from "jquery";
-import Popper from "popper.js";
 import "bootstrap";
 
 import { ApiUtil } from "./pano/js/api.util";
@@ -7,9 +5,6 @@ import { ApiUtil } from "./pano/js/api.util";
 import App from "./App.svelte";
 
 ApiUtil.init(process.env.API_URL);
-
-window.jQuery = window.jquery = window.$ = jquery;
-window.Popper = Popper;
 
 const app = new App({
   target: document.body,
