0bd2dafe9613462f101fc14dcd5499bf5cb69949
Selim Gökçek <selimgokcek@outlook.com>
2021-02-05 05:58:46 -0800

    Update settings page

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -10,38 +10,38 @@
   }
 </script>
 
-<!-- Settings Categories Nav -->
-<nav class="page-category">
-  <div class="nav nav-pills d-flex justify-content-center flex-row w-100">
-    <a
-      class="nav-item nav-link"
-      href="/panel/settings"
-      class:active="{matching($path, '/panel/settings')}">
-      Genel
-    </a>
-    <a
-      class="nav-item nav-link"
-      href="/panel/settings/site-settings"
-      class:active="{matching($path, '/panel/settings/site-settings', true)}">
-      Website
-    </a>
-    <a
-      class="nav-item nav-link ml-sm-auto position-relative"
-      href="/panel/settings/updates"
-      class:active="{matching($path, '/panel/settings/updates', true)}">
-      Güncellemeler
-      <span class="notification"></span>
-    </a>
-    <a
-      class="nav-item nav-link"
-      href="/panel/settings/about"
-      class:active="{matching($path, '/panel/settings/about', true)}">
-      Hakkında
-    </a>
-  </div>
-</nav>
-
 <!-- Settings Contents -->
 <article class="container">
+  <!-- Settings Categories Nav -->
+  <nav>
+    <div class="nav nav-pills d-flex justify-content-center flex-row w-100">
+      <a
+        class="nav-item nav-link"
+        href="/panel/settings"
+        class:active="{matching($path, '/panel/settings')}">
+        Genel
+      </a>
+      <a
+        class="nav-item nav-link"
+        href="/panel/settings/site-settings"
+        class:active="{matching($path, '/panel/settings/site-settings', true)}">
+        Website
+      </a>
+      <a
+        class="nav-item nav-link ml-sm-auto position-relative"
+        href="/panel/settings/updates"
+        class:active="{matching($path, '/panel/settings/updates', true)}">
+        Güncellemeler
+        <span class="notification"></span>
+      </a>
+      <a
+        class="nav-item nav-link"
+        href="/panel/settings/about"
+        class:active="{matching($path, '/panel/settings/about', true)}">
+        Hakkında
+      </a>
+    </div>
+  </nav>
+  <hr />
   <Router />
 </article>
