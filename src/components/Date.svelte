f17ef6a721955971579b5dcb61eac2b01b90a408
kahverengi <ahmet.duruer@yandex.com>
2021-02-17 13:00:33 +0300

    Add date component

---
+++ src/components/Date.svelte
@@ -0,0 +1,13 @@
+<script>
+  import moment from "moment";
+
+  import tooltip from "../pano/js/tooltip.util";
+
+  export let time;
+
+  $: date = time;
+</script>
+
+<span
+  use:tooltip="{['bottom', moment(parseInt(date)).format('DD/MM/YYYY, HH:mm')]}"
+  >{moment(parseInt(date)).calendar()}</span>
