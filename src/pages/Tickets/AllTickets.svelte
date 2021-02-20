7ba722c82346ff127fb4d21480e59e46d73d244b
kahverengi <ahmet.duruer@yandex.com>
2021-02-20 22:24:04 +0300

    Add title by page type in tickets page

--- src/pages/Tickets/AllTickets.svelte
+++ src/pages/Tickets/AllTickets.svelte
@@ -270,8 +270,7 @@
     <div class="row justify-content-between pb-3 align-items-center">
       <div class="col-md-6 col-12 text-md-left text-center">
         <h5 class="card-title mb-md-0">
-          {ticketsCount}
-          Talep{getListOfChecked($checkedList).length > 0
+          {ticketsCount} {pageType === "waitingReply" ? "Yeni" : pageType === "closed" ? "Kapalı": ""} Talep{getListOfChecked($checkedList).length > 0
             ? ", " + getListOfChecked($checkedList).length + " adet seçildi"
             : ""}
         </h5>
