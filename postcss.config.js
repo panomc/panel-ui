5c35bf76df8e5aaea4e4ace20cc96db412da080b
kahverengi <ahmet.duruer@yandex.com>
2021-03-26 15:02:20 +0300

    Remove purgecss due to broken compile

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,4 @@
 const production = !process.env.ROLLUP_WATCH;
-const purgecss = require("@fullhuman/postcss-purgecss");
 const autoprefixer = require("autoprefixer");
 // const postcssImport = require("postcss-import");
 const cssnano = require("cssnano");
@@ -8,11 +7,6 @@ module.exports = {
   plugins: [
     // postcssImport(),
     autoprefixer(),
-    production &&
-      purgecss({
-        content: ["./**/*.html", "./**/*.svelte", "./**/**/*.svelte"],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-      }),
     production &&
       cssnano({
         preset: "default",
