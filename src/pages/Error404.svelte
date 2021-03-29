ef1d0e4c75123cfe9fb47027ed8993dd246603ef
Selim Gökçek <selimgokcek@outlook.com>
2021-03-29 17:41:35 +0300

    Update Error404 page

--- src/pages/Error404.svelte
+++ src/pages/Error404.svelte
@@ -14,18 +14,10 @@
       class="img-fluid mb-3"
       src="/assets/img/404.png"
       width="280" />
-    <div class="card-body bg-lightprimary rounded">
-      <h2>Hata: 404</h2>
-      <p class="text-muted">
-        Yaramaz Enderman, bu sayfanın yüklenmesini engelledi.
-      </p>
-      <a class="btn btn-primary" role="button" href="./">Geri Git</a>
-      <a
-        class="btn btn-outline-primary disabled"
-        role="button"
-        href="./"
-        aria-disabled="true"
-        disabled>Hata Bildir</a>
-    </div>
+    <h2>Hata: 404</h2>
+    <p class="text-muted">
+      Yaramaz Enderman, bu sayfanın yüklenmesini engelledi.
+    </p>
+    <a class="btn btn-primary" role="button" href="./">Geri Git</a>
   </div>
 </div>
