517207cf03d426d72828981b0bc2313431963bc8
kahverengi <ahmet.duruer@yandex.com>
2021-11-25 14:28:23 +0100

    Better time format in Date component, resolves #39

--- src/components/Date.svelte
+++ src/components/Date.svelte
@@ -3,13 +3,17 @@
   use:tooltip="{[
     format(new Date(parseInt(date)), 'dd/MM/yyyy, HH:mm'),
     { placement: 'bottom' },
-  ]}">{format(new Date(parseInt(date)), "dd/MM/yyyy")}</span
+  ]}">{formatRelative(new Date(parseInt(date)), new Date()).capitalize()}</span
 >
 
 <script>
-  import { format } from "date-fns";
+  import { format, formatRelative } from "date-fns";
 
   import tooltip from "$lib/tooltip.util";
+
+  String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  };
 
   export let time;
 
