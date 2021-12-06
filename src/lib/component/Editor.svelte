3f62fc57978f1baa95116b26947015f7f91d4945
kahverengi <ahmet.duruer@yandex.com>
2021-12-06 22:37:29 +0100

    Add Tiptap editor component

---
+++ src/lib/component/Editor.svelte
@@ -0,0 +1,55 @@
+<style>
+  button.active {
+    background: black;
+    color: white;
+  }
+</style>
+
+{#if editor}
+  <button
+    on:click="{() => editor.chain().focus().toggleHeading({ level: 1 }).run()}"
+    class:active="{editor.isActive('heading', { level: 1 })}">
+    H1
+  </button>
+  <button
+    on:click="{() => editor.chain().focus().toggleHeading({ level: 2 }).run()}"
+    class:active="{editor.isActive('heading', { level: 2 })}">
+    H2
+  </button>
+  <button
+    on:click="{() => editor.chain().focus().setParagraph().run()}"
+    class:active="{editor.isActive('paragraph')}">
+    P
+  </button>
+{/if}
+
+<div bind:this="{element}"></div>
+
+<script>
+  import { onMount, onDestroy } from "svelte";
+  import { Editor } from "@tiptap/core";
+  import StarterKit from "@tiptap/starter-kit";
+
+  let element;
+  let editor;
+
+  export let content = ""
+
+  onMount(() => {
+    editor = new Editor({
+      element: element,
+      extensions: [StarterKit],
+      content: content,
+      onTransaction: () => {
+        // force re-render so `editor.isActive` works as expected
+        editor = editor;
+      },
+    });
+  });
+
+  onDestroy(() => {
+    if (editor) {
+      editor.destroy();
+    }
+  });
+</script>
--- src/routes/_PostEditor.svelte
+++ src/routes/_PostEditor.svelte
@@ -88,44 +88,7 @@
 
           <div class="align-selft-center w-100 h-75">
             <!-- Editor -->
-            <div id="editorToolbar">
-              <span class="ql-formats">
-                <select class="ql-size"></select>
-              </span>
-              <span class="ql-formats">
-                <button class="ql-bold"></button>
-                <button class="ql-italic"></button>
-                <button class="ql-underline"></button>
-                <button class="ql-strike"></button>
-              </span>
-              <span class="ql-formats">
-                <select class="ql-color"></select>
-                <select class="ql-background"></select>
-              </span>
-              <span class="ql-formats">
-                <button class="ql-header" value="1"></button>
-                <button class="ql-header" value="2"></button>
-                <button class="ql-blockquote"></button>
-                <button class="ql-code-block"></button>
-              </span>
-              <span class="ql-formats">
-                <button class="ql-list" value="ordered"></button>
-                <button class="ql-list" value="bullet"></button>
-                <button class="ql-indent" value="-1"></button>
-                <button class="ql-indent" value="+1"></button>
-              </span>
-              <span class="ql-formats">
-                <button class="ql-direction" value="rtl"></button>
-                <select class="ql-align"></select>
-              </span>
-              <span class="ql-formats">
-                <button class="ql-link"></button>
-                <button class="ql-image"></button>
-                <button class="ql-video"></button>
-              </span>
-            </div>
-
-            <div id="editor"></div>
+            <Editor content="{data.post.text}"/>
             <!-- Editor End -->
           </div>
         </div>
@@ -337,6 +300,8 @@
     show as showPostCategoriesAddEditModal,
     setCallback as setCallbackForPostCategoriesAddEditModal,
   } from "../components/modals/PostCategoriesAddEditModal.svelte";
+
+  import Editor from "$lib/component/Editor.svelte";
 
   export let data;
 
