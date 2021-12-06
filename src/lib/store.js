2f9f2c02ff0c542f312b85cd6968e5aec88af7a7
kahverengi <ahmet.duruer@yandex.com>
2021-12-06 23:37:33 +0100

    Remove store outside of lib

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,1 +1,126 @@
-export * from "../Store";
+import { get, writable } from "svelte/store";
+
+import { PanelSidebarStorageUtil } from "$lib/storage.util";
+
+export const options = Object.freeze({
+  DEFAULT_PAGE_TITLE: "Pano",
+});
+
+export const networkErrorCallbacks = writable([]);
+export const retryingNetworkErrors = writable(false);
+
+export const isSidebarOpen = writable(
+  PanelSidebarStorageUtil.isThereSideBarOpenStatus()
+    ? PanelSidebarStorageUtil.getSidebarOpenStatus()
+    : true
+);
+export const sidebarTabsState = writable(
+  PanelSidebarStorageUtil.isThereSideBarTabsState()
+    ? PanelSidebarStorageUtil.getSidebarTabsState()
+    : "website"
+);
+
+export const user = writable({});
+export const website = writable({});
+
+export const currentServerPlatformMatchKey = writable("");
+export const platformKeyRefreshedTime = writable(new Date().getTime());
+export const platformAddress = writable("");
+
+export const servers = writable([]);
+export const notificationsCount = writable(0);
+
+export const logoutLoading = writable(false);
+
+export const notLoggedIn = writable(false);
+
+export const pageTitle = writable(null);
+
+export function setDefaults() {
+  networkErrorCallbacks.set([]);
+  retryingNetworkErrors.set(false);
+  notLoggedIn.set(false);
+  logoutLoading.set(false);
+  pageTitle.set(null);
+}
+
+export function toggleSidebar() {
+  isSidebarOpen.update((value) => {
+    PanelSidebarStorageUtil.savePanelSidebarStorageUtil(!value);
+
+    return !value;
+  });
+}
+
+export function setSidebarTabsState(state) {
+  sidebarTabsState.set(state);
+
+  PanelSidebarStorageUtil.setSidebarTabsState(state);
+}
+
+export function showNetworkErrorOnCatch(callback, isErrorAlready = false) {
+  if (isErrorAlready) {
+    networkErrorCallbacks.update((value) => value.concat(callback));
+
+    return;
+  }
+
+  new Promise((resolve, reject) => {
+    callback(resolve, reject);
+  }).catch(() => {
+    networkErrorCallbacks.update((value) => value.concat(callback));
+  });
+}
+
+export function resumeAfterNetworkError() {
+  retryingNetworkErrors.set(true);
+
+  const currentList = get(networkErrorCallbacks).concat();
+  const calledList = [];
+
+  function check() {
+    let callbacksDone = true;
+
+    currentList.forEach((item) => {
+      if (calledList.indexOf(item) === -1) {
+        callbacksDone = false;
+      }
+    });
+
+    if (callbacksDone) {
+      retryingNetworkErrors.set(false);
+    }
+  }
+
+  currentList.forEach((callback) => {
+    new Promise((resolve, reject) => {
+      callback(resolve, reject);
+    })
+      .then(() => {
+        calledList.push(callback);
+
+        networkErrorCallbacks.update((list) =>
+          list.filter((item) => item !== callback)
+        );
+
+        check();
+      })
+      .catch(() => {
+        calledList.push(callback);
+
+        check();
+      });
+  });
+}
+
+export function initializeBasicData(data) {
+  notLoggedIn.set(false);
+
+  user.set(data.user);
+  website.set(data.website);
+  currentServerPlatformMatchKey.set(data.platform_server_match_key);
+  platformKeyRefreshedTime.set(data.platform_server_match_key_time_started);
+  platformAddress.set(data.platform_host_address);
+  servers.set(data.servers);
+  notificationsCount.set(data.notifications_count);
+}
