712d8498b97ee8960689ad06bd80380ae11fc7e2
kahverengi <ahmet.duruer@yandex.com>
2021-12-28 22:57:27 +0300

    Add UI_URL to environment variables

--- src/lib/variables.js
+++ src/lib/variables.js
@@ -1,4 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+export const UI_URL = import.meta.env.VITE_UI_URL;
 export const COOKIE_PREFIX = import.meta.env.VITE_COOKIE_PREFIX;
 
 export const CSRF_TOKEN_COOKIE_NAME = "CSRFToken";
