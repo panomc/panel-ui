e61527b1d54d55ec320d2391d573ff88ffa1f456
Selim Gökçek <selimgokcek@outlook.com>
2022-01-19 21:40:28 +0300

    Upgrade TicketStatus badges to Bs5

--- src/lib/component/TicketStatus.svelte
+++ src/lib/component/TicketStatus.svelte
@@ -1,29 +1,26 @@
 {#if status === TicketStatuses.NEW}
   <a href="{base}/tickets/waitingReply"
     ><span
-      class="badge badge-secondary badge-pill"
+      class="badge bg-secondary rounded-pill"
       use:tooltip="{['Tüm Yeni Talepleri Göster', { placement: 'bottom' }]}"
       >Yeni</span
-    ></a
-  >
+    ></a>
 {:else if status === TicketStatuses.REPLIED}
   <a href="{base}/tickets"
     ><span
-      class="badge badge-sunflower badge-pill"
+      class="badge bg-sunflower rounded-pill"
       use:tooltip="{[
         'Tüm Yanıtlanan Talepleri Göster',
         { placement: 'bottom' },
       ]}">Yanıtlandı</span
-    ></a
-  >
+    ></a>
 {:else if status === TicketStatuses.CLOSED}
   <a href="{base}/tickets/closed"
     ><span
-      class="badge badge-bittersweet badge-pill"
+      class="badge bg-bittersweet rounded-pill"
       use:tooltip="{['Tüm Kapalı Talepleri göster', { placement: 'bottom' }]}"
       >Kapalı</span
-    ></a
-  >
+    ></a>
 {/if}
 
 <script context="module">
