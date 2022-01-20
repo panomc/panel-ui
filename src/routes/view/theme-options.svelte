ce342cc5cd434bde260fac0d2f683e8ce504b80e
Selim Gökçek <selimgokcek@outlook.com>
2022-01-20 17:21:23 +0300

    Update theme-options page

--- src/routes/view/theme-options.svelte
+++ src/routes/view/theme-options.svelte
@@ -1,26 +1,18 @@
 <!-- Theme Options Categories -->
 <section>
   <div class="card">
-    <nav class="border-bottom border-glass rounded-top page-category">
-      <div class="nav nav-link d-flex flex-row w-100">
-        <a href="" class="nav-item nav-link active"> Yazılar </a>
-        <a href="" class="nav-item nav-link">Renkler</a>
-        <a href="" class="nav-item nav-link">Düzen</a>
-        <a href="" class="nav-item nav-link">Diğer</a>
-      </div>
-    </nav>
     <div class="card-body animate__animated animate__fadeIn">
       <div class="row justify-content-between pb-2">
-        <div class="col-6 text-left">
+        <div class="col-md-auto col-12 text-md-left text-center">
           <h5 class="card-title">Yazı Seçenekleri</h5>
         </div>
       </div>
       <form>
-        <div class="form-group row">
-          <label class="col-md-2 col-form-labeel" for="postAmount">
+        <div class="mb-3 row">
+          <label class="col-sm-3 col-form-label" for="postAmount">
             Yazı Adeti:
           </label>
-          <div class="col-md-10">
+          <div class="col">
             <select class="form-control" id="postAmount">
               <option id="setPostAmount1" selected>1</option>
               <option id="setPostAmount2">2</option>
@@ -30,73 +22,16 @@
             </select>
           </div>
         </div>
-        <div class="form-group row">
-          <label class="col-md-2 col-form-labeel" for="dateFormat">
+        <div class="mb-3 row">
+          <label class="col-sm-3 col-form-label" for="dateFormat">
             Tarih Formatı:
           </label>
-          <div class="col-md-10">
-            <div class="form-check custom-control custom-radio">
-              <input
-                checked
-                class="form-check-input custom-control-input"
-                id="dateFormat1"
-                name="dateFormats"
-                type="radio"
-                value="option1" />
-              <label
-                class="form-check-label custom-control-label"
-                for="dateFormat1">
-                01/01/2019
-              </label>
-            </div>
-            <div class="form-check custom-control custom-radio">
-              <input
-                class="form-check-input custom-control-input"
-                id="dateFormat2"
-                name="dateFormats"
-                type="radio"
-                value="option2" />
-              <label
-                class="form-check-label custom-control-label"
-                for="dateFormat2">
-                01.01.2019
-              </label>
-            </div>
-            <div class="form-check custom-control custom-radio">
-              <input
-                class="form-check-input custom-control-input"
-                id="dateFormat3"
-                name="dateFormats"
-                type="radio"
-                value="option3" />
-              <label
-                class="form-check-label custom-control-label"
-                for="dateFormat3">
-                01-01-2019
-              </label>
-            </div>
+          <div class="col">
+            <select class="form-control" id="dateFormat">
+              <option id="dateFormat1" selected>1</option>
+              <option id="dateFormat2">1</option>
+            </select>
           </div>
-        </div>
-        <div class="form-group row">
-          <label class="col-md-2 col-form-labeel" for="siteLanguageSwitch">
-            Dil Değişim Paneli:
-          </label>
-          <div class="col-md-10">
-            <div class="custom-control custom-checkbox">
-              <input
-                class="custom-control-input"
-                id="siteLanguageSwitch"
-                type="checkbox" />
-              <label class="custom-control-label" for="siteLanguageSwitch">
-                Göster
-              </label>
-            </div>
-          </div>
-        </div>
-        <div class="form-group pt-4 mb-0">
-          <button class="btn btn-secondary text-light" type="submit">
-            Değişiklikleri Kaydet
-          </button>
         </div>
       </form>
     </div>
@@ -106,5 +41,5 @@
 <script>
   import { pageTitle } from "$lib/store";
 
-  pageTitle.set("Tema Ayarları")
+  pageTitle.set("Tema Ayarları");
 </script>
