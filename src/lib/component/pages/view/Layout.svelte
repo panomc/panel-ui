0519dd9723e69a3def4f11b8615cfcf9c1e0022d
kahverengi <ahmet.duruer@yandex.com>
2022-02-19 00:00:54 +0300

    Move views layout page under components

---
+++ src/lib/component/pages/view/Layout.svelte
@@ -0,0 +1,39 @@
+<!-- View Page -->
+
+<!-- View Page Contents -->
+<article class="container">
+  <!-- View Categories Navigation -->
+  <nav>
+    <div class="nav nav-pills d-flex flex-row w-100 mb-3">
+      <a
+        class="nav-item nav-link"
+        href="{base}/view"
+        class:active="{matching($page.path, base + '/view')}"
+      >
+        Temalar
+      </a>
+      <a
+        class="nav-item nav-link"
+        href="{base}/view/theme-options"
+        class:active="{matching($page.path, base + '/view/theme-options')}"
+      >
+        Tema Seçenekleri
+      </a>
+    </div>
+  </nav>
+
+  <slot />
+</article>
+
+<script>
+  import { base } from "$app/paths";
+  import { page } from "$app/stores";
+
+  function matching(path, pathName, startsWith = false) {
+    return (
+      path.toUpperCase() === pathName.toUpperCase() ||
+      path.toUpperCase() === (pathName + "/").toUpperCase() ||
+      (startsWith && path.startsWith(pathName))
+    );
+  }
+</script>
--- src/routes/view/__layout.svelte
+++ src/routes/view/__layout.svelte
@@ -1,39 +1,7 @@
-<!-- View Page -->
-
-<!-- View Page Contents -->
-<article class="container">
-  <!-- View Categories Navigation -->
-  <nav>
-    <div class="nav nav-pills d-flex flex-row w-100 mb-3">
-      <a
-        class="nav-item nav-link"
-        href="{base}/view"
-        class:active="{matching($page.path, base + '/view')}"
-      >
-        Temalar
-      </a>
-      <a
-        class="nav-item nav-link"
-        href="{base}/view/theme-options"
-        class:active="{matching($page.path, base + '/view/theme-options')}"
-      >
-        Tema Seçenekleri
-      </a>
-    </div>
-  </nav>
-
+<Layout>
   <slot />
-</article>
+</Layout>
 
 <script>
-  import { base } from "$app/paths";
-  import { page } from "$app/stores";
-
-  function matching(path, pathName, startsWith = false) {
-    return (
-      path.toUpperCase() === pathName.toUpperCase() ||
-      path.toUpperCase() === (pathName + "/").toUpperCase() ||
-      (startsWith && path.startsWith(pathName))
-    );
-  }
+  import Layout from "$lib/component/pages/view/Layout.svelte";
 </script>
