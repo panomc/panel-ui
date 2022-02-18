9353882fa656d4f561742741b5ca7deaaaf73fc1
kahverengi <ahmet.duruer@yandex.com>
2022-02-18 23:43:58 +0300

    Move error page under components

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,25 +1,5 @@
-<!-- 404 Page  -->
-<div class="container">
-  <div
-    class="d-flex flex-column align-items-center justify-content-center mt-5
-    container animated bounceInDown"
-  >
-    <img
-      alt="Hata: 404"
-      class="img-fluid mb-3"
-      src="/assets/img/404.png"
-      width="280"
-    />
-    <h2>Hata: 404</h2>
-    <p class="text-muted">
-      Yaramaz Enderman, bu sayfanın yüklenmesini engelledi.
-    </p>
-    <a class="btn btn-primary" role="button" href="./">Geri Git</a>
-  </div>
-</div>
+<Error />
 
 <script>
-  import { pageTitle } from "$lib/store";
-
-  pageTitle.set("Error 404");
+  import Error from "$lib/component/pages/Error.svelte";
 </script>
