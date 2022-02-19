19231ca14681c3a160e3f8413c23fd0579885450
kahverengi <ahmet.duruer@yandex.com>
2022-02-19 11:37:03 +0300

    Move settings layout to under layouts

--- src/routes/settings/__layout.svelte
+++ src/routes/settings/__layout.svelte
@@ -1,62 +1,7 @@
-<!-- Settings Contents -->
-<article class="container">
-  <!-- Settings Categories Nav -->
-  <nav>
-    <div class="nav nav-pills d-flex justify-content-center flex-row w-100">
-      <a
-        class="nav-item nav-link"
-        href="{base}/settings"
-        class:active="{matching($page.path, base + '/settings')}"
-      >
-        Genel
-      </a>
-      <a
-        class="nav-item nav-link"
-        href="{base}/settings/site-settings"
-        class:active="{matching(
-          $page.path,
-          base + '/settings/site-settings',
-          true
-        )}"
-      >
-        Website
-      </a>
-      <a
-        class="nav-item nav-link border position-relative"
-        href="{base}/settings/updates"
-        class:active="{matching($page.path, base + '/settings/updates', true)}"
-      >
-        Güncellemeler
-        <span
-          class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle"
-        >
-          <span class="visually-hidden">New alerts</span>
-        </span>
-      </a>
-      <a
-        class="nav-item nav-link"
-        href="{base}/settings/about"
-        class:active="{matching($page.path, base + '/settings/about', true)}"
-      >
-        Hakkında
-      </a>
-    </div>
-  </nav>
-
-  <hr />
-
+<Layout>
   <slot />
-</article>
+</Layout>
 
 <script>
-  import { base } from "$app/paths";
-  import { page } from "$app/stores";
-
-  function matching(path, pathName, startsWith = false) {
-    return (
-      path.toUpperCase() === pathName.toUpperCase() ||
-      path.toUpperCase() === (pathName + "/").toUpperCase() ||
-      (startsWith && path.startsWith(pathName))
-    );
-  }
+  import Layout from "$lib/component/layouts/SettingsLayout.svelte";
 </script>
