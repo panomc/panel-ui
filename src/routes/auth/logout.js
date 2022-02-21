838a7be2ca6865c37de45702514368f022bb90a2
kahverengi <ahmet.duruer@yandex.com>
2022-02-21 22:41:19 +0300

    Remove unnecessary code from logout endpoint

--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -1,28 +1,24 @@
 import {
   COOKIE_PREFIX,
-  CSRF_HEADER,
   CSRF_TOKEN_COOKIE_NAME,
   JWT_COOKIE_NAME,
 } from "$lib/variables";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function post({ locals, request: { headers } }) {
-  return (
-    headers &&
-    headers.get(CSRF_HEADER) === locals.CSRFToken && {
-      headers: {
-        "set-cookie": [
-          `${
-            COOKIE_PREFIX + JWT_COOKIE_NAME
-          }=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
-          `${
-            COOKIE_PREFIX + CSRF_TOKEN_COOKIE_NAME
-          }=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
-        ],
-      },
-      body: {
-        result: "ok",
-      },
-    }
-  );
+export async function post() {
+  return {
+    headers: {
+      "set-cookie": [
+        `${
+          COOKIE_PREFIX + JWT_COOKIE_NAME
+        }=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+        `${
+          COOKIE_PREFIX + CSRF_TOKEN_COOKIE_NAME
+        }=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+      ],
+    },
+    body: {
+      result: "ok",
+    },
+  };
 }
