1d6d5c09c6bfc241cad3eeab73831c8d6728303d
kahverengi <ahmet.duruer@yandex.com>
2022-02-27 19:15:01 +0300

    Fix not bypassing testNotification endpoint

--- src/routes/api/testNotification.js
+++ src/routes/api/testNotification.js
@@ -1,8 +1,8 @@
 import * as api from "$lib/api.util.server";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get({ request }) {
-  const response = await api.get("/testNotification", await request.text());
+export async function get({ locals: { jwt } }) {
+  const response = await api.get("/testNotification", jwt);
 
   return {
     body: response,
