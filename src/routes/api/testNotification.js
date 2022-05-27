ef0343e1f0ed89326b9047f383a3fb80b3a1878c
kahverengi <ahmet@duruer.dev>
2022-05-27 15:36:28 +0200

    Improve proxying API requests

--- src/routes/api/testNotification.js
+++ src/routes/api/testNotification.js
@@ -1,8 +1,8 @@
 import * as api from "$lib/api.util.server";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get({ locals: { jwt } }) {
-  const response = await api.get("/testNotification", jwt);
+export async function get({ url, locals: { jwt } }) {
+  const response = await api.get(url.pathname + url.search, jwt);
 
   return {
     body: response,
