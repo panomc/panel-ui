5c811bb8b2095b02b6538e003bfd6e6d2d5974f0
Selim Gökçek <selimgokcek@outlook.com>
2022-06-16 21:42:29 +0300

    Update Sidebar navigation menu

--- src/lib/component/sidebar/Bottom.svelte
+++ src/lib/component/sidebar/Bottom.svelte
@@ -1,24 +1,21 @@
 <nav class="nav justify-content-around w-100 py-1 position-absolute bottom-0">
   <a
     class="nav-link text-light"
-    href="{base}/settings"
-    use:tooltip="{['Ayarlar', { placement: 'top' }]}"
-  >
-    <i class="fas fa-cog"></i>
+    href="{base}/help"
+    use:tooltip="{['Yardım', { placement: 'top' }]}">
+    <i class="fas fa-question-circle"></i>
   </a>
   <a
     class="nav-link text-light"
     href="javascript:void(0);"
-    use:tooltip="{['Pano Market', { placement: 'top' }]}"
-  >
+    use:tooltip="{['Pano Market', { placement: 'top' }]}">
     <i class="fas fa-store-alt"></i>
   </a>
   <a
     class="nav-link text-light"
     href="https://panomc.com/discord"
     target="_blank"
-    use:tooltip="{['Yardım', { placement: 'top' }]}"
-  >
+    use:tooltip="{['Yardım', { placement: 'top' }]}">
     <i class="fab fa-discord"></i>
   </a>
 </nav>
--- src/lib/component/sidebar/SiteNavigationMenu.svelte
+++ src/lib/component/sidebar/SiteNavigationMenu.svelte
@@ -1,72 +1,66 @@
 <nav
   class="sidebar-nav navbar-dark animate__animated animate__slideInLeft"
-  v-if="sidebarTabsState === 'website'"
->
-  <ul class="menu navbar-nav font-weight-normal">
-    <li class="nav-item" class:active="{matching($page.url.pathname, base)}">
+  v-if="sidebarTabsState === 'website'">
+  <ul class="navbar-nav px-3">
+    <li
+      class="nav-item p-2"
+      class:active="{matching($page.url.pathname, base)}">
       <a class="nav-link" href="{base}/">
-        <i class="fas fa-chart-line mx-2"></i>
+        <i class="fas fa-chart-pie me-2"></i>
         İstatistikler
       </a>
     </li>
 
     <li
-      class="nav-item"
-      class:active="{matching($page.url.pathname, base + '/posts', true)}"
-    >
-      <a class="nav-link" href="{base}/posts">
-        <i class="fa-solid fa-pen mx-2"></i>
+      class="nav-item p-2"
+      class:active="{matching($page.url.pathname, base + '/posts', true)}">
+      <a href="{base}/posts" class="nav-link">
+        <i class="fas fa-pen me-2"></i>
         Yazılar
       </a>
     </li>
-
     <li
-      class="nav-item"
-      class:active="{matching($page.url.pathname, base + '/tickets', true)}"
-    >
+      class="nav-item p-2"
+      class:active="{matching($page.url.pathname, base + '/tickets', true)}">
       <a class="nav-link" href="{base}/tickets">
-        <i class="fas fa-ticket-alt mx-2"></i>
+        <i class="fas fa-ticket me-2"></i>
         Talepler
       </a>
     </li>
 
     <li
-      class="nav-item"
-      class:active="{matching($page.url.pathname, base + '/players', true)}"
-    >
+      class="nav-item p-2"
+      class:active="{matching($page.url.pathname, base + '/players', true)}">
       <a class="nav-link" href="{base}/players">
-        <i class="fas fa-users mx-2"></i>
+        <i class="fas fa-users me-2"></i>
         Oyuncular
       </a>
     </li>
 
     <li
-      class="nav-item"
-      class:active="{matching($page.url.pathname, base + '/addons', true)}"
-    >
-      <a class="nav-link" href="{base}/addons">
-        <i class="fas fa-puzzle-piece mx-2"></i>
-        Eklentiler
-      </a>
-    </li>
-
-    <li
-      class="nav-item"
-      class:active="{matching($page.url.pathname, base + '/view', true)}"
-    >
+      class="nav-item p-2"
+      class:active="{matching($page.url.pathname, base + '/view', true)}">
       <a class="nav-link" href="{base}/view">
-        <i class="fas fa-palette mx-2"></i>
+        <i class="fas fa-palette me-2"></i>
         Görünüm
       </a>
     </li>
 
     <li
-      class="nav-item"
-      class:active="{matching($page.url.pathname, base + '/tools', true)}"
-    >
-      <a class="nav-link" href="{base}/tools">
-        <i class="fas fa-tools mx-2"></i>
-        Araçlar
+      class="nav-item p-2"
+      class:active="{matching($page.url.pathname, base + '/addons', true)}">
+      <a class="nav-link" href="{base}/addons">
+        <i class="fas fa-puzzle-piece me-2"></i>
+        Eklentiler
+      </a>
+    </li>
+
+    <li
+      class="nav-item p-2"
+      class:active="{matching($page.url.pathname, base + '/settings', true)}">
+      <a class="nav-link" href="{base}/settings">
+        <i class="fas fa-cog me-2"></i>
+        Ayarlar
       </a>
     </li>
   </ul>
