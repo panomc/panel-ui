cdc983729db5074806d4f94ad47ab638ae68b3fd
kahverengi <ahmet@duruer.dev>
2022-06-19 17:43:39 +0200

    Fix active effect on website navigation menu

--- src/lib/component/sidebar/SiteNavigationMenu.svelte
+++ src/lib/component/sidebar/SiteNavigationMenu.svelte
@@ -2,63 +2,70 @@
   class="sidebar-nav navbar-dark animate__animated animate__slideInLeft"
   v-if="sidebarTabsState === 'website'">
   <ul class="navbar-nav px-3">
-    <li
-      class="nav-item p-2"
-      class:active="{matching($page.url.pathname, base)}">
-      <a class="nav-link" href="{base}/">
+    <li class="nav-item p-2">
+      <a
+        class="nav-link"
+        href="{base}/"
+        class:active="{matching($page.url.pathname, base)}">
         <i class="fas fa-chart-pie me-2"></i>
         İstatistikler
       </a>
     </li>
 
-    <li
-      class="nav-item p-2"
-      class:active="{matching($page.url.pathname, base + '/posts', true)}">
-      <a href="{base}/posts" class="nav-link">
+    <li class="nav-item p-2">
+      <a
+        href="{base}/posts"
+        class="nav-link"
+        class:active="{matching($page.url.pathname, base + '/posts', true)}">
         <i class="fas fa-pen me-2"></i>
         Yazılar
       </a>
     </li>
-    <li
-      class="nav-item p-2"
-      class:active="{matching($page.url.pathname, base + '/tickets', true)}">
-      <a class="nav-link" href="{base}/tickets">
+    <li class="nav-item p-2">
+      <a
+        class="nav-link"
+        href="{base}/tickets"
+        class:active="{matching($page.url.pathname, base + '/tickets', true)}">
         <i class="fas fa-ticket me-2"></i>
         Talepler
       </a>
     </li>
 
-    <li
-      class="nav-item p-2"
-      class:active="{matching($page.url.pathname, base + '/players', true)}">
-      <a class="nav-link" href="{base}/players">
+    <li class="nav-item p-2">
+      <a
+        class="nav-link"
+        href="{base}/players"
+        class:active="{matching($page.url.pathname, base + '/players', true)}">
         <i class="fas fa-users me-2"></i>
         Oyuncular
       </a>
     </li>
 
-    <li
-      class="nav-item p-2"
-      class:active="{matching($page.url.pathname, base + '/view', true)}">
-      <a class="nav-link" href="{base}/view">
+    <li class="nav-item p-2">
+      <a
+        class="nav-link"
+        href="{base}/view"
+        class:active="{matching($page.url.pathname, base + '/view', true)}">
         <i class="fas fa-palette me-2"></i>
         Görünüm
       </a>
     </li>
 
-    <li
-      class="nav-item p-2"
-      class:active="{matching($page.url.pathname, base + '/addons', true)}">
-      <a class="nav-link" href="{base}/addons">
+    <li class="nav-item p-2">
+      <a
+        class="nav-link"
+        href="{base}/addons"
+        class:active="{matching($page.url.pathname, base + '/addons', true)}">
         <i class="fas fa-puzzle-piece me-2"></i>
         Eklentiler
       </a>
     </li>
 
-    <li
-      class="nav-item p-2"
-      class:active="{matching($page.url.pathname, base + '/settings', true)}">
-      <a class="nav-link" href="{base}/settings">
+    <li class="nav-item p-2">
+      <a
+        class="nav-link"
+        href="{base}/settings"
+        class:active="{matching($page.url.pathname, base + '/settings', true)}">
         <i class="fas fa-cog me-2"></i>
         Ayarlar
       </a>
