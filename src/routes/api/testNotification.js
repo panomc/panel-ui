22821c585c31701dbbe6458975b0abfdd9f19152
kahverengi <ahmet@duruer.dev>
2022-07-28 11:03:30 +0300

    Upgrade dependencies and convert rest methods to upper case

--- src/routes/api/testNotification.js
+++ src/routes/api/testNotification.js
@@ -1,7 +1,7 @@
 import * as api from "$lib/api.util.server";
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
-export async function get({ url, locals: { jwt } }) {
+export async function GET({ url, locals: { jwt } }) {
   const response = await api.get(url.pathname + url.search, jwt);
 
   return {
