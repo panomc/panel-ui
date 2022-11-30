88bac65299b560f535f084249932c0c435914f49
kahverengi <ahmet@duruer.dev>
2022-12-01 00:06:19 +0300

    Add permission group detail page

---
+++ src/lib/pages/players/PermissionGroupDetail.svelte
@@ -0,0 +1,15 @@
+Permission group detail
+
+<script context="module">
+  /**
+   * @type {import('@sveltejs/kit').PageLoad}
+   */
+  export async function load(event) {
+    const { parent } = event;
+    await parent();
+  }
+</script>
+
+<script>
+  export let data;
+</script>
