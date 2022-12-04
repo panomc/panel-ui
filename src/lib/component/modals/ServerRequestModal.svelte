8b1187bd0d3148d3f87bdb55527414e241bac4ea
Selim Gökçek <selimgokcek@outlook.com>
2022-12-04 20:13:43 +0300

    Design ServerRequestModal

--- src/lib/component/modals/ServerRequestModal.svelte
+++ src/lib/component/modals/ServerRequestModal.svelte
@@ -9,26 +9,53 @@
     <div class="modal-content">
       <div class="modal-body text-center">
         <div class="pb-3">
-          <i class="fas fa-question-circle fa-3x d-block m-auto text-gray"></i>
+          <i class="fas fa-plug fa-3x d-block m-auto text-gray"></i>
         </div>
-        Sunucu bağlantısı geldi pnp
+        X sunucusunu platforma bağlamak istediğinizden emin misiniz?
+        <div class="card mb-3 text-start">
+          <div class="row g-0">
+            <div
+              class="col-md-4 d-flex align-items-center justify-content-center">
+              <img
+                src="https://icons.iconarchive.com/icons/chrisl21/minecraft/64/Crafting-Table-icon.png"
+                class="img-fluid"
+                alt="SUNUCU ADI" />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-6">
+                    <h5 class="card-title">SUNUCU ADI</h5>
+                  </div>
+                  <div class="col-6 justify-content-start align-items-start">
+                    <small>Spigot</small>
+                  </div>
+                </div>
+                <p class="card-text">MOTD</p>
+                <p class="card-text">
+                  <small class="text-muted">X/X</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-footer">
+      <div class="modal-footer flex-nowrap">
         <button
-          class="btn btn-link text-muted"
+          class="btn btn-link col-6 m-0"
           type="button"
           class:disabled="{loading}"
           disabled="{loading}"
           on:click="{hide}">
-          Hayır
+          İptal
         </button>
         <button
-          class="btn btn-danger"
+          class="btn btn-secondary col-6 m-0"
           type="button"
           class:disabled="{loading}"
           disabled="{loading}"
           on:click="{onSubmit}">
-          Evet
+          Bağla
         </button>
       </div>
     </div>
@@ -69,7 +96,5 @@
 <script>
   let loading;
 
-  function onSubmit() {
-
-  }
+  function onSubmit() {}
 </script>
