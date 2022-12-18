9a86ff599e447ec0c1fa7a8c280176364126927b
kahverengi <ahmet@duruer.dev>
2022-12-18 17:23:13 +0300

    Add UI logic when a server is selected in server navigation menu

--- src/lib/component/sidebar/ServerNavigationMenu.svelte
+++ src/lib/component/sidebar/ServerNavigationMenu.svelte
@@ -1,22 +1,26 @@
 <nav class="sidebar-nav navbar-dark" v-if="sidebarTabsState === 'game'">
   <div class="container text-center animate__animated animate__zoomIn">
-    <i class="fas fa-cube mx-2 fa-3x m-3 text-light text-opacity-25"></i>
+    {#if $selectedServer}
+      Seçilmiş olan sunucu: {$selectedServer.name}
+    {:else}
+      <i class="fas fa-cube mx-2 fa-3x m-3 text-light text-opacity-25"></i>
 
-    <p class="text-gray">
-      Bağlı sunucu yok. Sunucu menüsünü görebilmek için sunucu bağlayın.
-    </p>
+      <p class="text-gray">
+        Bağlı sunucu yok. Sunucu menüsünü görebilmek için sunucu bağlayın.
+      </p>
 
-    <button
-      class="btn btn-secondary btn-sm"
-      data-bs-target="#connectServer"
-      data-bs-toggle="modal"
-      type="button">
-      <i class="fa-solid fa-plus me-2"></i>
-      Sunucu Bağla
-    </button>
+      <button
+        class="btn btn-secondary btn-sm"
+        data-bs-target="#connectServer"
+        data-bs-toggle="modal"
+        type="button">
+        <i class="fa-solid fa-plus me-2"></i>
+        Sunucu Bağla
+      </button>
+    {/if}
   </div>
 </nav>
 
 <script>
-  import { base } from "$app/paths";
+  import { selectedServer } from "$lib/Store.js";
 </script>
