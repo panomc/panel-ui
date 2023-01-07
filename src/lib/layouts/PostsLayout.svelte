675bd6cbd314cf4e835464b0ec2be72e0ed6396a
kahverengi <ahmet@duruer.dev>
2023-01-07 21:15:00 +0300

    Add permission requirement in post pages

--- src/lib/layouts/PostsLayout.svelte
+++ src/lib/layouts/PostsLayout.svelte
@@ -2,6 +2,22 @@
 
 <ConfirmDeletePostModal />
 
+<script context="module">
+  import { redirect } from "@sveltejs/kit";
+  import { hasPermission, Permissions } from "$lib/auth.util.js";
+
+  /**
+   * @type {import('@sveltejs/kit').LayoutLoad}
+   */
+  export async function load({ parent }) {
+    await parent();
+
+    if (!hasPermission(Permissions.MANAGE_POSTS)) {
+      throw redirect(302, "/");
+    }
+  }
+</script>
+
 <script>
   import ConfirmDeletePostModal from "$lib/component/modals/ConfirmDeletePostModal.svelte";
 </script>
