0dfc0293b3b62b52a60940ac2d66739c8563dc09
kahverengi <ahmet@duruer.dev>
2023-01-07 20:08:52 +0300

    Add auth util

---
+++ src/lib/auth.util.js
@@ -0,0 +1,23 @@
+import { user } from "$lib/Store.js";
+import { get } from "svelte/store";
+
+export const Permissions = Object.freeze({
+  ACCESS_PANEL: "access_panel",
+  MANAGE_SERVERS: "manage_servers",
+  MANAGE_POSTS: "manage_posts",
+  MANAGE_TICKETS: "manage_tickets",
+  MANAGE_PLAYERS: "manage_players",
+  MANAGE_VIEW: "manage_view",
+  MANAGE_ADDON: "manage_addon",
+  MANAGE_PLATFORM_SETTINGS: "manage_platform_settings"
+});
+
+export function hasPermission(permission) {
+  const userObject = get(user)
+
+  if (userObject.admin) {
+    return true;
+  }
+
+  return userObject.permissions.includes(permission)
+}
