b1993f9c6683fb931a1b28f378b39e20f92d4eb9
Selim Gökçek <selimgokcek@tuta.io>
2023-01-10 01:36:08 +0300

    Design ServerSettings page

--- src/lib/pages/server/ServerSettings.svelte
+++ src/lib/pages/server/ServerSettings.svelte
@@ -1,1 +1,26 @@
-Server settings page
+<!-- Site Settings Sub Page -->
+<div class="tab-pane">
+  <div class="card">
+    <div class="card-body animate__animated animate__fadeIn">
+        <h5 class="card-title">CraftBukkit (localhost:25564)</h5>
+      <div class="row mb-3">
+        <label class="col-md-4 col-form-label" for="mainServer">
+          Ana Sunucu
+        </label>
+        <div class="col col-form-label">
+            <p class="mb-0 text-muted"><i class="fa-solid fa-check me-2"></i> CraftBukkit ana sunucu</p>
+            <small class="text-muted d-block">Websitede görüntülenecek sunucu bilgilerini ana sunucu belirler.</small>
+            <a href="#" class="btn btn-link ps-0"><i class="fa-solid fa-home me-2"></i> Ana Sunucu Belirle</a>
+        </div>
+      </div>
+      <div class="row py-3 rounded my-3 bg-danger bg-opacity-25">
+        <label class="col-md-4 col-form-label" for="removeServer">
+          Sunucuyu Kaldır
+        </label>
+        <div class="col d-flex align-items-center">
+          <a href="#" class="btn btn-link link-danger ps-0"><i class="fa-solid fa-plug me-2"></i> Bağlantıyı Kes</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
