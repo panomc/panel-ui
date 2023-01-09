418149af26691d1c849ae278b2e9846037e1bec7
Selim Gökçek <selimgokcek@tuta.io>
2023-01-09 21:36:24 +0300

    Remove ServerDashboard actions section

--- src/lib/pages/server/ServerDashboard.svelte
+++ src/lib/pages/server/ServerDashboard.svelte
@@ -1,21 +1,6 @@
 <div class="container">
 
   <div
-    class="row justify-content-end mb-3 animate__animated animate__slideInUp">
-    <div class="col-auto">
-      <a href="#" class="btn btn-link" role="button" 
-            use:tooltip="{['Yenile', { placement: 'bottom' }]}">
-        <i class="fa-solid fa-sync fa-spin"></i>
-        <!-- yenilendiginde fa-spin classi'i ve disabled eklenecek -->
-      </a>
-    </div>
-    <div class="col-auto">
-      <a href="#" class="btn btn-outline-primary ml-auto" role="button">
-        <i class="fa-solid fa-house me-2"></i> Ana Sunucu Yap
-      </a>
-    </div>
-</div>
-  <div
     class="row justify-content-between mb-3 animate__animated animate__slideInUp">
     <div class="col-4">
       <div class="card bg-secondary">
