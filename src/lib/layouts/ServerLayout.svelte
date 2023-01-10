173df61b24187e4141b0b7b6a2b7d61845866616
kahverengi <ahmet@duruer.dev>
2023-01-10 20:00:14 +0300

    Add redirect for server layout if any server is not selected

--- src/lib/layouts/ServerLayout.svelte
+++ src/lib/layouts/ServerLayout.svelte
@@ -3,14 +3,20 @@
 <script context="module">
   import { redirect } from "@sveltejs/kit";
   import { hasPermission, Permissions } from "$lib/auth.util.js";
+  import { get } from "svelte/store";
+  import { selectedServer } from "$lib/Store.js";
 
   /**
    * @type {import('@sveltejs/kit').LayoutLoad}
    */
   export async function load({ parent }) {
-    await parent();
+    const parentData = await parent();
 
     if (!hasPermission(Permissions.MANAGE_SERVERS)) {
+      throw redirect(302, "/");
+    }
+
+    if (!get(selectedServer)) {
       throw redirect(302, "/");
     }
   }
