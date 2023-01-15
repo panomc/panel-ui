1292539861d2318558ecfe9d4253b411c77f351a
kahverengi <ahmet@duruer.dev>
2023-01-15 20:25:21 +0300

    Add page title for game integration settings page

--- src/lib/pages/server/GameIntegrationSettings.svelte
+++ src/lib/pages/server/GameIntegrationSettings.svelte
@@ -1,1 +1,9 @@
 Game integration settings page
+
+<script>
+  import { getContext } from "svelte";
+
+  const pageTitle = getContext("pageTitle");
+
+  pageTitle.set("Oyun Entegrasyon Ayarları");
+</script>
