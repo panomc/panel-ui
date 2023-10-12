ff71f4025d0bae5dee538ecbc784a98a75885c39
kahverengi <ahmet@duruer.dev>
2023-10-12 19:54:45 +0300

    Translate ticket status badge component

--- src/lib/component/badges/TicketStatusBadge.svelte
+++ src/lib/component/badges/TicketStatusBadge.svelte
@@ -1,17 +1,17 @@
 {#if status === TicketStatuses.NEW}
   <a href="{base}/tickets/waitingReply"
-    ><span class="badge bg-success text-white rounded-pill" title="Filtrele"
-      >Yeni</span
+    ><span class="badge bg-success text-white rounded-pill" title="{$_('components.ticket-status-badge.filter')}"
+      >{$_('components.ticket-status-badge.new')}</span
     ></a>
 {:else if status === TicketStatuses.REPLIED}
   <a href="{base}/tickets"
-    ><span class="badge bg-warning text-white rounded-pill" title="Filtrele"
-      >Yanıtlandı</span
+    ><span class="badge bg-warning text-white rounded-pill" title="{$_('components.ticket-status-badge.filter')}"
+      >{$_('components.ticket-status-badge.replied')}</span
     ></a>
 {:else if status === TicketStatuses.CLOSED}
   <a href="{base}/tickets/closed"
-    ><span class="badge bg-danger text-white rounded-pill" title="Filtrele"
-      >Kapalı</span
+    ><span class="badge bg-danger text-white rounded-pill" title="{$_('components.ticket-status-badge.filter')}"
+      >{$_('components.ticket-status-badge.closed')}</span
     ></a>
 {/if}
 
@@ -24,6 +24,8 @@
 </script>
 
 <script>
+  import { _ } from "svelte-i18n";
+
   import { base } from "$app/paths";
 
   export let status = TicketStatuses.NEW;
