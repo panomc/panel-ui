b4a790880bd1394e66b3f924b5bdcebdcbf9677f
kahverengi <ahmet@duruer.dev>
2023-10-12 19:51:47 +0300

    Translate Date component

--- src/lib/component/Date.svelte
+++ src/lib/component/Date.svelte
@@ -1,23 +1,24 @@
-<!--  .format('DD/MM/YYYY, HH:mm')-->
-
 <span
   use:tooltip="{[
     format(new Date(parseInt(date)), 'dd/MM/yyyy, HH:mm'),
-    { placement: 'bottom' },
-  ]}">
+    { placement: 'bottom', locale: locales[$currentLanguage['date-fns-code']] },
+  ]}"
+  class="text-muted">
   <slot>
     {#if relativeFormat}
-      {formatRelative(new Date(parseInt(date)), new Date()).capitalize()}
+      {formatRelative(new Date(parseInt(date)), new Date(), {locale: locales[$currentLanguage['date-fns-code']]}).capitalize()}
     {:else}
-      {format(new Date(parseInt(date)), "dd MMMM yyyy")}
+      {format(new Date(parseInt(date)), "dd MMMM yyyy", {locale: locales[$currentLanguage['date-fns-code']]})}
     {/if}
   </slot>
 </span>
 
 <script>
   import { format, formatRelative } from "date-fns";
+  import * as locales from 'date-fns/locale'
 
   import tooltip from "$lib/tooltip.util";
+  import { currentLanguage } from "$lib/language.util.js";
 
   String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
