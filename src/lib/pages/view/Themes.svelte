204a5d367f38729a5238f727bae6e1c2c53c8082
Selim Gökçek <selimgokcek@outlook.com>
2023-10-13 23:58:06 +0300

    Remove unnecessary css

--- src/lib/pages/view/Themes.svelte
+++ src/lib/pages/view/Themes.svelte
@@ -1,32 +1,13 @@
 <!-- Theme Settings -->
-<section class="row">
-  <div class="col-lg-4 col-md-6">
-    <div class="card selected animate__animated animate__fadeIn">
-      <img
-        alt="Vanilla"
-        class="rounded p-2"
-        height="auto"
-        src="{base}/assets/img/vanilla.png"
-        width="100%" />
-      <div class="card-body">
-        <div class="row justify-content-between align-items-end">
-          <div class="col-auto">
-            <h5 class="card-title mb-0">Vanilla</h5>
-            Yapımcı: Butlu
-          </div>
-          <div class="col-auto">
-            <button
-              class="btn btn-outline-danger btn-sm"
-              data-bs-target="#confirmDeleteTheme"
-              data-bs-toggle="modal">
-              Kaldır
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+<div class="card col-4 bg-white animate__animated animate__fadeIn">
+  <img alt="Vanilla" class="card-img-top" src="{base}/assets/img/vanilla.png" />
+  <div class="card-body">
+    <h5 class="card-title mb-0">Vanilla</h5>
+    <p class="card-text">
+      <small class="text-muted">Yapımcı: Butlu</small>
+    </p>
   </div>
-</section>
+</div>
 
 <ConfirmDeleteThemeModal />
 
