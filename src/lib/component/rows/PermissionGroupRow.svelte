c604a49f7e28a23c50a83c07d917a9eb2e781df1
kahverengi <ahmet@duruer.dev>
2023-10-14 00:11:22 +0300

    Translated permission group row component

--- src/lib/component/rows/PermissionGroupRow.svelte
+++ src/lib/component/rows/PermissionGroupRow.svelte
@@ -5,14 +5,14 @@
         class="btn btn-sm btn-link link-danger"
         href="javascript:void(0);"
         on:click="{onDeleteClick}"
-        title="Sil">
+        title="{$_('components.permission-group-row.delete')}">
         <i class="fas fa-trash"></i>
       </a>
     {/if}
   </th>
   <td class="align-middle text-nowrap">
     <a
-      title="Düzenle"
+      title="{$_('components.permission-group-row.edit')}"
       href="{base}/players/perm-groups/detail/{permissionGroup.id}"
       >{permissionGroup.name}</a>
   </td>
@@ -25,6 +25,7 @@
 
 <script>
   import { createEventDispatcher } from "svelte";
+  import { _ } from "svelte-i18n";
 
   import { base } from "$app/paths";
 
