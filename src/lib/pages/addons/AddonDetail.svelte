20b48b6d2acda3b00f187e3eb553e2cd325bef6b
kahverengi <ahmet@duruer.dev>
2024-03-07 17:39:15 +0300

    create addon detail page

---
+++ src/lib/pages/addons/AddonDetail.svelte
@@ -0,0 +1,11 @@
+<div class="container">
+  Test addon detail page
+</div>
+
+<script>
+  import { getContext } from "svelte";
+
+  const pageTitle = getContext("pageTitle");
+
+  pageTitle.set("Eklenti Detayı");
+</script>
---
+++ src/routes/addons/[addonId]/+page.svelte
@@ -0,0 +1,5 @@
+<AddonDetail />
+
+<script>
+  import AddonDetail from "$lib/pages/addons/AddonDetail.svelte";
+</script>
