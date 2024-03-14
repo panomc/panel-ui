b9e4ad471434aada08186b223e80c924b5e2a20a
slmgkck <selimgokcek@outlook.com>
2024-03-14 17:33:38 +0300

    Improve PageAction layout

--- src/lib/component/PageActions.svelte
+++ src/lib/component/PageActions.svelte
@@ -1,6 +1,11 @@
-
 <div class="row mb-3 align-items-center animate__animated animate__slideInUp">
-  <div class="col-lg-4"><slot name="left"/></div>
-  <div class="col-lg-4"><slot name="middle"/></div>
-  <div class="col-lg-4 d-flex justify-content-end"><slot name="right"/></div>
+  <div class="col-xl-4 col-6 order-1">
+    <slot name="left" />
+  </div>
+  <div class="col-xl-4 col-12 order-xl-2 order-first">
+    <slot name="middle" />
+  </div>
+  <div class="col-xl-4 col-6 d-flex justify-content-end order-3">
+    <slot name="right" />
+  </div>
 </div>
