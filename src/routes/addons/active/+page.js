62f7ff5c4283d46a0abf3e77c0ea2161206c7d16
kahverengi <ahmet@duruer.dev>
2024-06-01 13:38:57 +0300

    add addons page ui logic

---
+++ src/routes/addons/active/+page.js
@@ -0,0 +1,8 @@
+import { load as loadAddons, PageTypes } from "$lib/pages/Addons.svelte";
+
+/**
+ * @type {import('@sveltejs/kit').PageLoad}
+ */
+export async function load(params) {
+  return loadAddons(params, PageTypes.ACTIVE);
+}
